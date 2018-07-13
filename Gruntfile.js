8dfd2f97de839605504145e41ea356e30fd76891
Jason Mulligan <jason.mulligan@avoidwork.com>
2018-07-13 07:23:46 -0400

    Removing ES5 transpile, major version bump to handle the syntax change
    Updating engineStrict value

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,18 +20,7 @@ module.exports = function (grunt) {
 					"src/factory.js",
 					"src/outro.js"
 				],
-				dest: "lib/<%= pkg.name %>.es6.js"
-			}
-		},
-		babel: {
-			options: {
-				sourceMap: false,
-				presets: ["env"]
-			},
-			dist: {
-				files: {
-					"lib/<%= pkg.name %>.js": "lib/<%= pkg.name %>.es6.js"
-				}
+				dest: "lib/<%= pkg.name %>.js"
 			}
 		},
 		eslint: {
@@ -44,23 +33,6 @@ module.exports = function (grunt) {
 		},
 		nodeunit: {
 			all: ["test/*.js"]
-		},
-		uglify: {
-			options: {
-				banner: '/* <%= grunt.template.today("yyyy") %> <%= pkg.author %> */\n',
-				sourceMap: true,
-				sourceMapIncludeSources: true,
-				mangle: {
-					reserved: [
-						"TinyStack"
-					]
-				}
-			},
-			target: {
-				files: {
-					"lib/<%= pkg.name %>.min.js": ["lib/<%= pkg.name %>.js"]
-				}
-			}
 		},
 		watch: {
 			js: {
@@ -78,23 +50,22 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks("grunt-contrib-concat");
 	grunt.loadNpmTasks("grunt-contrib-nodeunit");
 	grunt.loadNpmTasks("grunt-contrib-watch");
-	grunt.loadNpmTasks("grunt-contrib-uglify");
 	grunt.loadNpmTasks("grunt-babel");
 	grunt.loadNpmTasks("grunt-eslint");
 
 	grunt.task.registerTask("babili", "Minifies ES2016+ code", function () {
 		const fs = require("fs"),
 			path = require("path"),
-			data = fs.readFileSync(path.join(__dirname, "lib", "tiny-stack.es6.js"), "utf8").replace("\"use strict\";", ""), // Stripping "use strict"; because it's injected
+			data = fs.readFileSync(path.join(__dirname, "lib", "tiny-stack.js"), "utf8").replace("\"use strict\";", ""), // Stripping "use strict"; because it's injected
 			pkg = require(path.join(__dirname, "package.json")),
 			banner = "/*\n " + new Date().getFullYear() + " " + pkg.author + "\n @version " + pkg.version + "\n*/\n\"use strict\";";
 
 		try {
-			const minified = require("babel-core").transform(data, {sourceFileName: "tiny-stack.es6.js", sourceMaps: true, presets: ["minify"]});
+			const minified = require("babel-core").transform(data, {sourceFileName: "tiny-stack.js", sourceMaps: true, presets: ["minify"]});
 
-			fs.writeFileSync(path.join(__dirname, "lib", "tiny-stack.es6.min.js"), banner + minified.code + "\n//# sourceMappingURL=tiny-stack.es6.min.js.map", "utf8");
+			fs.writeFileSync(path.join(__dirname, "lib", "tiny-stack.min.js"), banner + minified.code + "\n//# sourceMappingURL=tiny-stack.min.js.map", "utf8");
 			grunt.log.ok("1 file created.");
-			fs.writeFileSync(path.join(__dirname, "lib", "tiny-stack.es6.min.js.map"), JSON.stringify(minified.map), "utf8");
+			fs.writeFileSync(path.join(__dirname, "lib", "tiny-stack.min.js.map"), JSON.stringify(minified.map), "utf8");
 			grunt.log.ok("1 sourcemap created.");
 		} catch (e) {
 			console.error(e.stack || e.message || e);
@@ -104,6 +75,6 @@ module.exports = function (grunt) {
 
 	// aliases
 	grunt.registerTask("test", ["eslint", "nodeunit"]);
-	grunt.registerTask("build", ["concat", "babel", "babili", "uglify"]);
+	grunt.registerTask("build", ["concat", "babili"]);
 	grunt.registerTask("default", ["build", "test"]);
 };
